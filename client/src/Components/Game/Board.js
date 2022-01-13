e752a366476d349eeae9d74f50558bd243e646a5
Ryan Hafner <r.hafner20@gmail.com>
2022-01-12 23:06:43 -0500

    replace BoardRow with RowContainer styled-component

--- client/src/Components/Game/Board.js
+++ client/src/Components/Game/Board.js
@@ -6,28 +6,6 @@ import { CenteredFlex, Flex } from "../Styles/Flex";
 
 import { LetterSlot } from "./LetterSlot";
 
-const RowContainer = styled(Flex).attrs(() => ({
-	align: "center"
-}))`
-	&:not(:first-of-type) {
-		border-top: 2px solid rgba(0,0,0,0.05);
-	}
-`;
-
-function BoardRow({ content = [] }) {
-	return (
-		<RowContainer>
-			{content.map(({ letter, status }, i) => (
-				<LetterSlot
-					key={i}
-					letter={letter}
-					status={status}
-				/>
-			))}
-		</RowContainer>
-	)
-}
-
 const Container = styled(CenteredFlex).attrs(() => ({
 	column: true
 }))`
@@ -36,12 +14,21 @@ const Container = styled(CenteredFlex).attrs(() => ({
 	box-shadow: inset 0px 3px 11px rgba(0,0,0,0.3);
 `;
 
+const RowContainer = styled(Flex).attrs(() => ({
+	align: "center"
+}))`
+	&:not(:first-of-type) {
+		border-top: 2px solid rgba(0,0,0,0.05);
+	}
+`;
+
 const HiddenInput = styled.input`
 	display: none;
 `;
 
 const allowed = "abcdefghijklmnopqrstuvwxyz".split("");
-export function Board({ enabled, gameState, onInput, onBackspace, onEnter }) {
+
+export function Board({ enabled, gameState, onInput, onBackspace, onEnter, size }) {
 	const [ inputEl, setInputEl ] = useState();
 
 	useEffect(() => {
@@ -67,7 +54,16 @@ export function Board({ enabled, gameState, onInput, onBackspace, onEnter }) {
 	return (
 		<Container onClick={() => inputEl && inputEl.click()}>
 			{gameState.map((row, i) => (
-				<BoardRow key={i} content={row} />
+				<RowContainer key={i}>
+					{row.map(({ letter, status }, r) => (
+						<LetterSlot
+							key={r}
+							letter={letter}
+							status={status}
+							size={size}
+						/>
+					))}
+				</RowContainer>
 			))}
 			{isMobile && <HiddenInput ref={setInputEl}/>}
 		</Container>
